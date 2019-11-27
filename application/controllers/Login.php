042c23358585dc3dfeb1a0a86b9a1c883d91b20a
MilzamHutomo <milzamhutomo.social@gmail.com>
2019-11-27 23:20:19 +0700

    Add rules and validation for login input

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -12,10 +12,15 @@ class Login extends CI_Controller
 
 	public function index()
 	{
+		// Rules for registration form
 		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[customer.mail]', [
 			'is_unique' => 'This e-mail has been registered !'
 		]);
 		$this->form_validation->set_rules('register_password', 'Password', 'required|trim|min_length[8]');
+
+		// Rules for login form
+		$this->form_validation->set_rules('login_email', 'Email', 'required|trim|valid_email');
+		$this->form_validation->set_rules('login_password', 'Password', 'required|trim');
 
 		if ($this->form_validation->run() == false) {
 			$this->load->view('layout/header_page');
@@ -27,10 +32,15 @@ class Login extends CI_Controller
 				'password' => password_hash($this->input->post('register_password'), PASSWORD_DEFAULT)
 			];
 
+			// Insert data to connected database
 			$this->db->insert('customer', $data);
+
+			// Create notification when successfully registered
 			$this->session->set_flashdata('message', '<div class="alert alert-success text-center" role="alert">
 			Account Successfully Registered!
-		 	</div>');
+			 </div>');
+
+			// Page to open after creating account
 			redirect(base_url('login'));
 		}
 	}
