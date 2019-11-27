32bdc88dd9d8e808aa97a5af7db61260f342b13d
MilzamHutomo <milzamhutomo.social@gmail.com>
2019-11-27 22:50:04 +0700

    Final login data connection to database

--- application/views/pages/login_register.php
+++ application/views/pages/login_register.php
@@ -15,6 +15,7 @@
 <!-- Breadcrumb area End -->
 
 <!-- Main Content Wrapper Start -->
+
 <div class="main-content-wrapper">
     <div class="page-content-inner pt--75 pb--80">
         <div class="container">
@@ -43,6 +44,7 @@
                 <div class="col-md-6">
                     <div class="register-box">
                         <h4 class="heading__tertiary mb--30">Register</h4>
+                        <?= $this->session->flashdata('message') ?>
                         <form class="form form--register" method="post" action="<?= base_url('login') ?>">
                             <div class="form__group mb--20">
                                 <label class="form__label" for="email">Email address <span class="required">*</span></label>
