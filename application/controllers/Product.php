f1de6d8294fcbb32019b2dd970c540a222bc0ea8
Satria Dewo <satriadewoo@gmail.com>
2019-12-02 15:55:53 +0700

    Add to Cart

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -3,13 +3,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Product extends CI_Controller
 {
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('mProduct');
+	}
 
 	public function index()
 	{
+		$data['product'] = $this->mProduct->get_all_product()->result();
 		$data['customer'] = $this->db->get_where('customer', ['mail' => $this->session->userdata('mail')])->row_array();
 		$customer['user_name'] = $data['customer']['mail'];
+
+
 		$this->load->view('layout/header_page', $customer);
-		$this->load->view('pages/product');
+		$this->load->view('pages/product', $data);
 		$this->load->view('layout/footer');
 	}
 }
